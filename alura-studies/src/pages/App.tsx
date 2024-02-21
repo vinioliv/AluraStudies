7f0880e575fec1edd19dbdfb306e34100dc926f2
Vinicius Oliveira <vinicius.oliveira148571@gmail.com>
2024-02-21 18:13:02 -0300

    feat: Interligando o formulario e a lista

--- alura-studies/src/pages/App.tsx
+++ alura-studies/src/pages/App.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
 import style from './App.module.css';
+import { Cronometro } from '../components/Cronometro';
+import { ITarefa } from '../types/tarefa';
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
   return (
     <div className={style.AppStyle}>
-      <Formulario/>
-      <Lista/>
+      <Formulario setTarefas={setTarefas}/>
+      <Lista tarefas={tarefas}/>
+      <Cronometro/>
     </div>
   );
 }
