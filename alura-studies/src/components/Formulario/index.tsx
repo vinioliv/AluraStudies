93fc2949fddb5d394454045c4b7b56b39f38e25c
Vinicius Oliveira <vinicius.oliveira148571@gmail.com>
2024-02-21 18:12:06 -0300

    feat: recebendo os valores do formulario e adicionando na lista

--- alura-studies/src/components/Formulario/index.tsx
+++ alura-studies/src/components/Formulario/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import Botao from '../Botao';
 import style from'./Formulario.module.css'
+import { ITarefa } from '../../types/tarefa';
 
-class Formulario extends React.Component{
+class Formulario extends React.Component<{
+    setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}>{
+    state = {
+        tarefa: "",
+        tempo: "00:00:00"
+    }
+
+    adicionarTarefa(evento:React.FormEvent<HTMLFormElement>){
+        evento.preventDefault();
+        this.props.setTarefas(tarefasAntigas => [...tarefasAntigas, {...this.state}])
+        this.setState({
+            tarefa: "",
+            tempo: "00:00"
+        })
+    }
+
     render(){
         return(
-           <form className={style.novaTarefa}>
+           <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor='tarefa'>
                         Adicione um novo estudo
@@ -14,6 +31,8 @@ class Formulario extends React.Component{
                     type='text'
                     name='tarefa'
                     id='tarefa'
+                    value={this.state.tarefa}
+                    onChange={evento => this.setState({...this.state, tarefa: evento.target.value})}
                     placeholder='O que você quer estudar'
                     required
                     />
@@ -27,6 +46,8 @@ class Formulario extends React.Component{
                     type='time'
                     step='1'
                     name='tempo'
+                    value={this.state.tempo}
+                    onChange={evento => this.setState({...this.state, tempo: evento.target.value})}
                     id='tempo'
                     min='00:00:00'
                     max='01:30:00'
@@ -34,7 +55,7 @@ class Formulario extends React.Component{
                     />
 
                 </div>
-                <Botao>
+                <Botao type="submit">
                     Adicionar
                 </Botao>
            </form>
