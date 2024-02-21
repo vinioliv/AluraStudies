a2abea0220929d541936c8375da687dd1ed64328
Vinicius Oliveira <vinicius.oliveira148571@gmail.com>
2024-02-21 18:11:03 -0300

    feat: componente relogio

---
+++ alura-studies/src/components/Cronometro/Relogio/index.tsx
@@ -0,0 +1,13 @@
+import style from './Relogio.module.css';
+
+export default function Relogio(){
+    return(
+     <>
+        <span className={style.relogioNumero}>0</span>
+        <span className={style.relogioNumero}>0</span>
+        <span className={style.relogioDivisao}>:</span>
+        <span className={style.relogioNumero}>0</span>
+        <span className={style.relogioNumero}>0</span>
+     </>
+    )
+}
