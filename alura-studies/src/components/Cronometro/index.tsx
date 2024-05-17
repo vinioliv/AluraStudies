81f90114003e425fa01f65408fd22da6eb8d04f7
Vinicius Oliveira <vinicius.oliveira@soc.com.br>
2024-05-17 16:20:33 -0300

    Adicionando som ao termino da tarefa

--- alura-studies/src/components/Cronometro/index.tsx
+++ alura-studies/src/components/Cronometro/index.tsx
@@ -5,6 +5,12 @@ import { tempoParaSegundos } from "../common/utils/time";
 import { ITarefa } from "../../types/tarefa";
 import { useEffect, useState } from "react";
 
+import alarm from "../../../src/assets/sounds/stop.mp3"
+
+import useSound from 'use-sound';
+
+
+
 interface Props{
     selecionado:  ITarefa | undefined,
     finalizarTarefa: () => void
@@ -12,33 +18,66 @@ interface Props{
 
 export function Cronometro({selecionado, finalizarTarefa}:Props){
     const[tempo, setTempo] = useState<number>();
+    const[cronometroRodando, setCronometroRodando] = useState<boolean>(false);
+   
+
+    const [playSound] = useSound(alarm);
+
     
+
+
     useEffect(()=>{
         if(selecionado?.tempo){
             setTempo(tempoParaSegundos(selecionado.tempo))
         }
     },[selecionado]);
 
-    function regressiva(contador: number = 0){
-        setTimeout(() => {
-            if(contador > 0){
-                setTempo(contador - 1);
-                return regressiva(contador - 1);
-            }
-            finalizarTarefa();
-        }, 1000)
+    useEffect(() => {
+        let timeoutId: NodeJS.Timeout;
+        
+    
+        if (cronometroRodando) {
+          const runTimer = () => {
+            timeoutId = setTimeout(() => {
+              setTempo((prevTempo) => {
+                if (prevTempo === 0) {
+                  setCronometroRodando(false); 
+                  finalizarTarefa();
+                  playSound();
+                } else if(prevTempo !== undefined){
+                  return prevTempo - 1;
+                }
+              });
+              runTimer();
+            }, 1000);
+          };
+    
+          runTimer();
+        }
+    
+        return () => clearTimeout(timeoutId);
+      }, [cronometroRodando]);
+   
+    function iniciarOuRetomarCronometro(){
+        setCronometroRodando(true)
     }
 
+    function pausarCronometro(){
+        setCronometroRodando(false)
+    }
+
+
+
     return (
-        <div className={style.cronometro}>
+        <div className={style.cronometro}> 
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
             <div className={style.relogioWrapper}>
             <Relogio tempo={tempo}/>
             </div>
-            <Botao onClick={ () => regressiva(tempo)} type="submit">
-                Começar!
-               
+            <Botao onClick={ () => cronometroRodando ? pausarCronometro() : iniciarOuRetomarCronometro()} type="submit">
+                {cronometroRodando ? `Pausar` : `Começar`}          
             </Botao>
+           
         </div>
     )
 }
